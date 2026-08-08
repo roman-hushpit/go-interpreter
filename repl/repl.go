7d471c5ce0db72696003723395b6d7ecf3d77304
Roman <roman.gushpit@gmail.com>
2026-08-08 10:30:58 +0000

    Add integer and boolean evaluations

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"go-interpreter/evaluator"
 	"go-interpreter/lexer"
 	"go-interpreter/parser"
 	"io"
@@ -26,8 +27,12 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
