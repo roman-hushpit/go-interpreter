98a94d03bc6b80edac6aaa17527e18ecadd14037
Roman <roman.gushpit@gmail.com>
2026-08-12 20:31:56 +0000

    Implement simple compiler function

--- code/code.go
+++ code/code.go
@@ -1,11 +1,44 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
 
 type Instructions []byte
+
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+		operands, read := ReadOperands(def, ins[i+1:])
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+		i += 1 + read
+	}
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
+	operandCount := len(def.OperandWidths)
+
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n",
+			len(operands), operandCount)
+	}
+
+	switch operandCount {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
+}
 
 type Opcode byte
 
@@ -14,8 +47,8 @@ const (
 )
 
 type Definition struct {
-	Name         string
-	OperandWidth []int
+	Name          string
+	OperandWidths []int
 }
 
 var definitions = map[Opcode]*Definition{
@@ -37,7 +70,7 @@ func Make(op Opcode, orerands ...int) []byte {
 	}
 
 	instructionLen := 1
-	for _, w := range def.OperandWidth {
+	for _, w := range def.OperandWidths {
 		instructionLen += w
 	}
 
@@ -46,7 +79,7 @@ func Make(op Opcode, orerands ...int) []byte {
 
 	offset := 1
 	for i, o := range orerands {
-		width := def.OperandWidth[i]
+		width := def.OperandWidths[i]
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
@@ -55,4 +88,21 @@ func Make(op Opcode, orerands ...int) []byte {
 	}
 
 	return instruction
+}
+
+func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+		offset += width
+	}
+	return operands, offset
+}
+
+func ReadUint16(ins []byte) uint16 {
+	return binary.BigEndian.Uint16(ins)
 }
