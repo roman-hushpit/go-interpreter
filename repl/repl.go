3766de186d041710c0ac97b70266db5ab7be5c01
Roman <roman.gushpit@gmail.com>
2026-08-13 10:46:02 +0000

    Support pop operation for stack in VM and compiler

--- repl/repl.go
+++ repl/repl.go
@@ -40,7 +40,7 @@ func Start(in io.Reader, out io.Writer) {
 			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
 			continue
 		}
-		stackTop := machine.StackTop()
+		stackTop := machine.LastPoppedStackElement()
 		io.WriteString(out, stackTop.Inspect())
 		io.WriteString(out, "\n")
 	}
