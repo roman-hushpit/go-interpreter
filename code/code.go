9c400b2cf32baf6576b94c976b417923add39353
Roman <roman.gushpit@gmail.com>
2026-08-17 17:49:06 +0000

    Add support for index operations

--- code/code.go
+++ code/code.go
@@ -37,6 +37,7 @@ const (
 
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -72,6 +73,7 @@ var definitions = map[Opcode]*Definition{
 
 	OpArray: {"OpArray", []int{2}},
 	OpHash:  {"OpHash", []int{2}},
+	OpIndex: {"OpIndex", []int{}},
 }
 
 func (ins Instructions) String() string {
